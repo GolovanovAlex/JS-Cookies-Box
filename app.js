d52408470f340366de97cbb243f56f7aff326fbd
GolovanovAlex <Golovanov.a.a@yandex.ru>
2023-01-10 10:40:27 +0300

    js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // ---- ---- Const ---- ---- //
 const cookiesBox = document.querySelector('.wrapper'),
   buttons = document.querySelectorAll('.button');
+
+// ---- ---- Show ---- ---- //
 const executeCodes = () => {
   cookiesBox.classList.add('show');
+
+  // ---- ---- Button ---- ---- //
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      cookiesBox.classList.remove('show');
+    });
+  });
 };
 
 window.addEventListener('load', executeCodes);
