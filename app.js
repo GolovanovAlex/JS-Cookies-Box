2c088377ca9008f37f2fcf9e7c9b191f721552f5
GolovanovAlex <Golovanov.a.a@yandex.ru>
2023-01-10 10:51:25 +0300

    fin

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const cookiesBox = document.querySelector('.wrapper'),
 
 // ---- ---- Show ---- ---- //
 const executeCodes = () => {
+  if (document.cookie.includes('AlexGolovanov')) return;
   cookiesBox.classList.add('show');
 
   // ---- ---- Button ---- ---- //
   buttons.forEach((button) => {
     button.addEventListener('click', () => {
       cookiesBox.classList.remove('show');
+
+      // ---- ---- Time ---- ---- //
+      if (button.id == 'acceptBtn') {
+        document.cookie =
+          'cookieBy= AlexGolovanov; max-age=' + 60 * 60 * 24 * 30;
+      }
     });
   });
 };
